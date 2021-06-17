06ab813b52c288db9663fe2405b54349d197c782
daeyoung14.kim <daeyoung14.kim@lge.com>
2021-06-17 15:45:09 +0900

    Initial commit

---
+++ app/src/main/java/com/practice/kunnyskotlin/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.practice.kunnyskotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.practice.kunnyskotlin.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
